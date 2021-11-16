3f568a59dd5e1c3d9b96f4f3c8c49172fd9e079b
Richard Dutra <80215713+richarddutra110@users.noreply.github.com>
2021-11-15 22:55:42 -0300

    Add files via upload

---
+++ scripts/entradaviateclado/calculadora.js
@@ -0,0 +1,10 @@
+var num1 = parseInt(prompt('digite o num 1'));
+var num2 = parseInt(prompt('digite o num 2'));
+
+var soma = num1+num2;
+
+document.getElementById('num1').innerHTML = "num1";
+document.getElementById('num2').innerHTML = "num2";
+document.getElementById('soma').innerHTML = "soma";
+
+//console.log(soma);
---
+++ scripts/variaveis/ex1.js
@@ -0,0 +1,9 @@
+var texto= "vamos fazer uma calculadora";
+var num1 = 25;
+var num2 = 27;
+soma = num1+num2
+
+//document.write('texto');
+alert('olá mundo');
+document.getElementById('teste').innerHTML = "texto";
+console.log('num1');
